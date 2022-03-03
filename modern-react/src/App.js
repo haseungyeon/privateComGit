71225e8fb812d87e82e47a791cb5ef5bcaa0e545
gktmdudl <gktmdudl@korea.ac.kr>
2022-03-03 16:37:03 +0900

    Input Added

--- modern-react/src/App.js
+++ modern-react/src/App.js
@@ -2,17 +2,20 @@
 // import './App.css';
 
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper'
-import HelloClass from './HelloClass'
-import Blank from './Blank'
+import Hello from './high/middle1/Hello';
+import Wrapper from './high/Wrapper';
+import PMButton from './high/PMButton';
+import HelloClass from './high/middle1/HelloClass'
+import Blank from './high/middle1/Blank'
 
 function App() {
   const name = 'Seungyeon';
+  const nick = 'bobo';
   return (
     <div className="hi">
+      <PMButton/>
       <Wrapper>
-        <Hello name={name} color="red"></Hello>
+        <Hello name={name} color="red" call={nick} isSpecial={true}></Hello>
         <Hello color='pink'></Hello>
       </Wrapper>
       {/* <Blank></Blank>
